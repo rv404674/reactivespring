e628d9148e925df2b4833be6c5ef20ad3158c713
Rahul Verma <rv404674@gmail.com>
2021-05-25 10:50:48 +0530

    Added junit test cases related to Flux

--- src/test/java/com/learnreactivespring/learnreactivespring/fluxandmongoplayground/FluxAndMonoTest.java
+++ src/test/java/com/learnreactivespring/learnreactivespring/fluxandmongoplayground/FluxAndMonoTest.java
@@ -1,9 +1,14 @@
 package com.learnreactivespring.learnreactivespring.fluxandmongoplayground;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(FluxAndMonoTest.class);
 
     @Test
     public void fluxTest(){
@@ -11,6 +16,7 @@ public class FluxAndMonoTest {
         // Basically we are throwing Runtime Exception and using concatwith, we are attaching it to the flux.
         // log will print what is happening behind the scenes.
         // NOTE: Once OnError is sent, flux wont sent any more messages.
+
         Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
                 //.concatWith(Flux.error(new RuntimeException("Exception Occurred")))
                 .concatWith(Flux.just("after on error"))
@@ -19,6 +25,7 @@ public class FluxAndMonoTest {
         // Now you will start receiving values from flux. Flux is useless if you don't subscribe.
         // :: is lambda
         // "Flux completed successfully will be printed once we recieve the onComplete() event".
+        logger.info("*** Inside the fluxTest Method");
         (stringFlux).
                 subscribe(System.out::println,
                         (e) -> System.err.println("Exception is" + e),
@@ -35,6 +42,58 @@ public class FluxAndMonoTest {
 //        20:06:35.646 [main] INFO reactor.Flux.Array.1 - | onNext(Reactive Spring)
 //        Reactive Spring
 //        20:06:35.647 [main] INFO reactor.Flux.Array.1 - | onComplete()
+    }
 
+    @Test
+    public void fluxTestElementsWithoutError(){
+        Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring").log();
+        // to check each element in the flux, use StepVerifier.
+
+        // Verify Complete is what will make stringFlux start emiting elements.
+        // verifyComplete ~= Subscribe
+        logger.info("*** Inside the fluxTestElementsWithoutError method");
+        StepVerifier.create(stringFlux)
+                .expectNext("Spring")
+                .expectNext("Spring Boot")
+                .expectNext("Reactive Spring")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxTestElementsWithError(){
+
+        logger.info("*** Inside the fluxTestElementsWithError ");
+        Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Spring", "Spring Boot", "Reactive Spring")
+                .expectErrorMessage("Exception Occured")
+                //.expectError(RuntimeException.class)
+                .verify();
+                //verifyComplete();
+
+        // NOTE:
+        // if we do verifyComplete() in the end, we will get thi
+        //  expectation "expectComplete" failed (expected: onComplete(); actual: onError
+        // Basically, we will be recieveing onError() because of the exception, where as we are asserting with
+        // verifyComplete() which expects an onComplete(). Hence change it.
+    }
+
+    /**
+     * Assert on the number of elements
+     */
+    @Test
+    public void fluxTestElementsCountWithError(){
+        logger.info("*** Inside the fluxTestElementsCountWithError");
+        Flux <String> stringFlux = Flux.just("Spring", "SpringBoot", "Reactive Spring")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(3)
+                .expectErrorMessage("Exception Occurrred")
+                .verify();
     }
 }
