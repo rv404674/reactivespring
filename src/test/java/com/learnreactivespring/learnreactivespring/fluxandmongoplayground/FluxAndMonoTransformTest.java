ff2ea30dfec1b8d64d669415c30604974333f96f
Rahul Verma <rv404674@gmail.com>
2021-06-01 20:12:30 +0530

    Used flatMap

--- src/test/java/com/learnreactivespring/learnreactivespring/fluxandmongoplayground/FluxAndMonoTransformTest.java
+++ src/test/java/com/learnreactivespring/learnreactivespring/fluxandmongoplayground/FluxAndMonoTransformTest.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static reactor.core.scheduler.Schedulers.parallel;
+
 public class FluxAndMonoTransformTest {
     List<String> names = new ArrayList<>(Arrays.asList("Rahul", "Sureshama", "Shubham Theja"));
 
@@ -56,6 +58,8 @@ public class FluxAndMonoTransformTest {
     public void transformUsingMapAndFilter(){
         // NOTE:
         // when you are performing multiple operations on a flux, it is called a pipeline.
+        // also it is recommended to use filter before map. Because obviously filter will reduce
+        // number of elements hence will reduce the overall complexity.
         Flux<String> stringFlux = Flux.fromIterable(names)
                 .filter(s -> s.length()>6)
                 .map(s -> s.toLowerCase())
@@ -65,5 +69,82 @@ public class FluxAndMonoTransformTest {
         StepVerifier.create(stringFlux)
                 .expectNext("sureshama", "shubham theja", "sureshama", "shubham theja") // because of the repeat
                 .verifyComplete();
+    }
+
+    @Test
+    public void transformUsingFlatMap(){
+        /**
+         * NOTE: you use flatMap when you have to call a db/external service for every element.
+         * basically for async things you use flatMap.
+         * NOTE:
+         * 1. FlatMap creates a new stream from every upstream event.
+         * 2. Subscribe to each newly created stream eagerly.
+         * 3. Dynamically merge all the newly created streams as they emit. This is called flatMap.
+         */
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F")) // A, B, C, D, E, F
+                .flatMap(s -> {
+                            return Flux.fromIterable(convertToList(s)); // A -> List[A, newValue], B-> List[B, newValue]
+                        }) // convertToList is mimicking an external service/db that returns a flux
+                .log();
+
+        // If you see we have 6 different list [A, newValue], [B, newValue] ...
+        // but flatMap flattens all of them, basically merge all of them
+        // NOTE: it will take 6sec.
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void transformUsingFlatMap_usingParallel(){
+        /**
+         * Doing the above function parrallely - subscribeOn
+         */
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F")) // Flux<String>
+                .window(2) // Flux< Flux<String> > ->(A,B), (B,C)
+                .flatMap((s) ->
+                        s.map(this::convertToList).subscribeOn(parallel())) // Flux<List<String>>
+                .flatMap(s -> Flux.fromIterable(s)) // Flux<String>
+                .log();
+
+        // If you see we have 6 different list [A, newValue], [B, newValue] ...
+        // but flatMap flattens all of them, basically merge all of them
+        // NOTE: it will take 6sec.
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void transformUsingFlatMap_maintainingOrderParallel(){
+        /**
+         * Doing the above function thing inOrder - concatMap, but it still took 6 sec
+         * use flatMapSequential - will run parrallely and give result in order (2s)
+         */
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F")) // Flux<String>
+                .window(2) // Flux< Flux<String> > ->(A,B), (B,C)
+                .flatMapSequential((s) ->
+                        s.map(this::convertToList).subscribeOn(parallel())) // Flux<List<String>>
+                .flatMap(s -> Flux.fromIterable(s)) // Flux<String>
+                .log();
+
+        // If you see we have 6 different list [A, newValue], [B, newValue] ...
+        // but flatMap flattens all of them, basically merge all of them
+        // NOTE: it will take 6sec.
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+
+    }
+
+    private List<String> convertToList(String s){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Arrays.asList(s, "newValue");
     }
 }
