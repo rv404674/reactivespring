103883673b4d6829a450abd76ed203f0b1467135
Rahul Verma <rv404674@gmail.com>
2021-08-19 07:58:47 +0530

    Added Rest Endpoints

---
+++ src/main/java/com/learnreactivespring/learnreactivespring/v1/ItemController.java
@@ -0,0 +1,24 @@
+package com.learnreactivespring.learnreactivespring.v1;
+
+import com.learnreactivespring.learnreactivespring.constants.ItemConstants;
+import com.learnreactivespring.learnreactivespring.model.Item;
+import com.learnreactivespring.learnreactivespring.repository.ItemReactiveRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@Slf4j
+public class ItemController {
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    @GetMapping(ItemConstants.ITEM_END_POINT_V1)
+    public Flux<Item> getAllItems(){
+        return itemReactiveRepository.findAll();
+    }
+
+}
