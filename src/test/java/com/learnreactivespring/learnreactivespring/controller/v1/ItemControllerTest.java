bcf3558877b4d4c9c3f6c724a2626431bcc57f34
Rahul Verma <rv404674@gmail.com>
2021-08-30 17:00:54 +0530

    Added a class to test ItemController

---
+++ src/test/java/com/learnreactivespring/learnreactivespring/controller/v1/ItemControllerTest.java
@@ -0,0 +1,64 @@
+package com.learnreactivespring.learnreactivespring.controller.v1;
+
+import com.learnreactivespring.learnreactivespring.constants.ItemConstants;
+import com.learnreactivespring.learnreactivespring.model.Item;
+import com.learnreactivespring.learnreactivespring.repository.ItemReactiveRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+public class ItemControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    public List<Item> data(){
+        return Arrays.asList(
+                new Item(null, "SamSung WashingMachine", 51000.0),
+                new Item(null, "Mobile Phone", 15000.0),
+                new Item("ABC", "watch", 1000.0)
+        );
+    }
+
+    @Before
+    public void setUp(){
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(data()))
+                .flatMap(s -> itemReactiveRepository.save(s))
+                .doOnNext((item -> {
+                    System.out.println("Inserted Item is: " + item);
+                }))
+                .blockLast();
+    }
+
+    @Test
+    public void getAllItems(){
+        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class)
+                .hasSize(3);
+    }
+
+
+
+}
